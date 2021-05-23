932b67c8030db9b961d5bd0a182c37dd1898fbc3
keerthivasan16 <42330364+keerthivasan16@users.noreply.github.com>
2021-05-23 11:14:58 +0530

    init

---
+++ practice/npattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{   
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;++i)
+    {
+        
+        for(int j=n-1-i;j>0;--j)
+            printf(" ");
+        //    
+        for(int k=0; k<(2*i)+1; ++k)
+            printf("%d ", abs(k-i));
+        printf("\n");
+        
+    }
+    
+
+}
